c37636390e684ba0493b86ce5534d824c8529ab9
Admin <Admin@DESKTOP-UJRSTTQ>
2024-11-07 15:01:25 +0530

    testcases written

--- framework/src/test/java/amazonlogout/Tocreateloogout.java
+++ framework/src/test/java/amazonlogout/Tocreateloogout.java
@@ -3,7 +3,7 @@ package amazonlogout;
 public class Tocreateloogout {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("logout succesful");
 
 	}
 
